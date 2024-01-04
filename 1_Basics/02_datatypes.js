a9972d37259ba3b5b091514a8cebdb2b502ff554
Anurag Borah <anuragborah2548@gmail.com>
2024-01-04 13:52:04 +0530

    datatypes

---
+++ 1_Basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; // Treats the code as newer version of JavaScript.
+
+/*
+Primitive Data Types
+*/
+
+// String
+let name = 'John Doe'
+
+// Number
+let age = 25
+
+// Boolean
+let isEligible = true
+
+// Undefined
+let address
+
+// Null
+let phoneNumber = null
+
+// Symbol
+let id = Symbol('id')
+
+// BigInt
+let bigNumber = 1234567890123456789012345678901234567890n
+
+console.table([name, age, isEligible, address, phoneNumber, id, bigNumber])
+
+console.log([typeof name, typeof age, typeof isEligible, typeof address, typeof id, typeof bigNumber]);
+
+/*
+Objects
+*/
+
+console.log(typeof phoneNumber);
