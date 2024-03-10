e9b65ace0613f4b0c8fcc713a8b512b7321a8b70
Anurag <anuragborah2548@gmail.com>
2024-03-10 21:03:14 +0000

    switch

--- 3_Control_Flow/02_switch.js
+++ 3_Control_Flow/02_switch.js
@@ -1,1 +1,30 @@
-switch (key)
+// Switch statement
+
+let day = 2;
+switch (day) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+// If you don't put break statement, it will execute all the cases after the matching case, except for the default case.
